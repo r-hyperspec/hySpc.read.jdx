1c07a1cb84b0fc991e83eb51ac8af14ccd642473
Bryan <hanson@depauw.edu>
2021-08-04 21:34:52 -0500

    fix DESCRIPTION misc format issues, move unit tests before function definition

--- R/read_jdx.R
+++ R/read_jdx.R
@@ -35,49 +35,9 @@
 #' file <- system.file("extdata", "SBO.jdx", package = "readJDX")
 #' spc <- read_jdx(file)
 #' plot(spc)
-read_jdx <- function(file = stop("filename is needed"), SOFC = TRUE, debuglevel = 0) {
-  list_jdx <- readJDX(file = file, SOFC = SOFC, debug = debuglevel)
-
-  # Extract labels
-  x_units <- jdx_extract_value(list_jdx$metadata, key = "XUNITS")
-  y_units <- jdx_extract_value(list_jdx$metadata, key = "YUNITS")
-
-  if (length(list_jdx) == 4) {
-    # Case 1: A single spectrum (IR, Raman, UV, processed/real 1D NMR, etc)
-    spc <- new("hyperSpec",
-      spc = list_jdx[[4]][["y"]],
-      wavelength = list_jdx[[4]][["x"]],
-      labels = list(.wavelength = x_units, spc = y_units)
-    )
-  }
-
-  # Not sure this next option will be of great interest to most hyperSpec users,
-  # but it works
-  else if (length(list_jdx) == 5) {
-    # Case 2: Includes spectrum and the real and imaginary parts of
-    #         1D NMR spectrum
-    temp_spc <- rbind(list_jdx[[4]]$y, list_jdx[[5]]$y)
-    spc <- new("hyperSpec",
-      spc = temp_spc,
-      wavelength = list_jdx[[4]]$x,
-      labels = list(.wavelength = x_units, spc = y_units)
-    )
-  } else {
-    stop(
-      "read_jdx() cannot process all types of JCAMP-DX files.\n",
-      "You may wish to look at readJDX::readJDX() for more information. \n",
-      "If you have a file you think should work, or wish to suggest an ",
-      "enhancement, please, create an issue at\n",
-      packageDescription("hySpc.read.jdx")$BugReports
-    )
-  }
-
-  # Output
-  .spc_io_postprocess_optional(spc, filename = file)
-}
-
-
+#'
 #' @tests testthat
+#'
 #' # get data files
 #' sbo <- system.file("extdata", "SBO.jdx", package = "readJDX")
 #' pcrf <- system.file("extdata", "PCRF.jdx", package = "readJDX")
@@ -119,3 +79,44 @@ read_jdx <- function(file = stop("filename is needed"), SOFC = TRUE, debuglevel 
 #'   expect_equal(as.character(labels(spc, ".wavelength")), "1/CM") # x units
 #' })
 #' 
+read_jdx <- function(file = stop("filename is needed"), SOFC = TRUE, debuglevel = 0) {
+  list_jdx <- readJDX(file = file, SOFC = SOFC, debug = debuglevel)
+
+  # Extract labels
+  x_units <- jdx_extract_value(list_jdx$metadata, key = "XUNITS")
+  y_units <- jdx_extract_value(list_jdx$metadata, key = "YUNITS")
+
+  if (length(list_jdx) == 4) {
+    # Case 1: A single spectrum (IR, Raman, UV, processed/real 1D NMR, etc)
+    spc <- new("hyperSpec",
+      spc = list_jdx[[4]][["y"]],
+      wavelength = list_jdx[[4]][["x"]],
+      labels = list(.wavelength = x_units, spc = y_units)
+    )
+  }
+
+  # Not sure this next option will be of great interest to most hyperSpec users,
+  # but it works
+  else if (length(list_jdx) == 5) {
+    # Case 2: Includes spectrum and the real and imaginary parts of
+    #         1D NMR spectrum
+    temp_spc <- rbind(list_jdx[[4]]$y, list_jdx[[5]]$y)
+    spc <- new("hyperSpec",
+      spc = temp_spc,
+      wavelength = list_jdx[[4]]$x,
+      labels = list(.wavelength = x_units, spc = y_units)
+    )
+  } else {
+    stop(
+      "read_jdx() cannot process all types of JCAMP-DX files.\n",
+      "You may wish to look at readJDX::readJDX() for more information. \n",
+      "If you have a file you think should work, or wish to suggest an ",
+      "enhancement, please, create an issue at\n",
+      packageDescription("hySpc.read.jdx")$BugReports
+    )
+  }
+
+  # Output
+  .spc_io_postprocess_optional(spc, filename = file)
+}
+
