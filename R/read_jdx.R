1bbcaebadf202d6e625f8778909d05122a71042c
Bryan <hanson@depauw.edu>
2021-08-13 14:25:09 -0700

    fix problems with UT

--- R/read_jdx.R
+++ R/read_jdx.R
@@ -34,46 +34,50 @@
 #'
 #' @tests testthat
 #'
-#' # get data files
 #' sbo <- system.file("extdata", "SBO.jdx", package = "readJDX")
-#' pcrf <- system.file("extdata", "PCRF.jdx", package = "readJDX")
-#' pcrf_265 <- system.file("extdata", "PCRF_line265.jdx", package = "readJDX")
-#' isasspc1 <- system.file("extdata", "isasspc1.dx", package = "readJDX")
 #'
 #' test_that("SBO.jdx (IR spectrum) can be imported", {
 #'   expect_silent(spc <- read_jdx(sbo))
 #' })
 #'
 #' test_that("SBO.jdx (IR spectrum) dimensions are correct", {
+#'   spc <- read_jdx(sbo)
 #'   expect_equal(nwl(spc), 1868L)
 #'   expect_equal(nrow(spc), 1L)
 #'   expect_equal(ncol(spc), 2L)
 #' })
 #'
 #' test_that("SBO.jdx (IR spectrum) labels are correct", {
+#'   spc <- read_jdx(sbo)
 #'   expect_equal(spc@label$.wavelength, expression("1/CM"))
 #'   expect_equal(spc@label$spc, expression("TRANSMITTANCE"))
-#'   expect_equal(basename(spc@label$filename), "SBO.jdx")
+#'   expect_equal(spc@label$filename, "filename")
 #' })
+#'
+#' pcrf <- system.file("extdata", "PCRF.jdx", package = "readJDX")
 #'
 #' test_that("PCRF.jdx (real & imaginary 1H NMR spectrum) can be imported", {
 #'   expect_silent(spc <- read_jdx(pcrf))
 #' })
 #'
 #' test_that("PCRF.jdx (real & imaginary 1H NMR spectrum) dimensions are correct", {
-#'   expect_silent(spc <- read_jdx(pcrf))
+#'   spc <- read_jdx(pcrf)
 #'   expect_equal(nwl(spc), 7014L)
 #'   expect_equal(nrow(spc), 2L)
 #'   expect_equal(ncol(spc), 2L)
 #' })
 #'
 #' test_that("PCRF.jdx (real & imaginary 1H NMR spectrum) labels are correct", {
+#'   spc <- read_jdx(pcrf)
 #'   expect_equal(spc@label$.wavelength, expression())
 #'   expect_equal(spc@label$spc, expression())
-#'   expect_equal(basename(spc@label$filename), "PCRF.jdx")
+#'   expect_equal(spc@label$filename, "filename")
 #' })
 #'
 #' # next test is for a corrupted file, which on readJDX throws an error
+#'
+#' pcrf_265 <- system.file("extdata", "PCRF_line265.jdx", package = "readJDX")
+#'
 #' test_that("Error is thrown when corrupt file is imported", {
 #'   expect_output(
 #'   expect_error(spc <- read_jdx(pcrf_265)),
@@ -84,10 +88,14 @@
 #' # next test is a 2D NMR file which readJDX imports fine, but
 #' # length(list_jdx) != 4 or 5, which is error condition for read_jdx
 #' # readJDX will issue a warning first, and then read_jdx throws an error
+#'
+#' isasspc1 <- system.file("extdata", "isasspc1.dx", package = "readJDX")
+#'
 #' test_that("Error is thrown when 2D NMR file is imported", {
-#'   expect_error(spc <- read_jdx(isasspc1),
-#'   "read_jdx() cannot process all types of JCAMP-DX files"
-#'   )
+#' expect_warning(
+#' expect_error(spc <- read_jdx(isasspc1)),
+#' "Looks like 2D NMR but could not identify vendor"
+#' )
 #' })
 #'
 #'
@@ -103,7 +111,7 @@ read_jdx <- function(file = stop("filename is needed"), SOFC = TRUE, debuglevel 
     spc <- new("hyperSpec",
       spc = list_jdx[[4]][["y"]],
       wavelength = list_jdx[[4]][["x"]],
-      labels = list(.wavelength = x_units, spc = y_units, filename = file)
+      labels = list(.wavelength = x_units, spc = y_units)
     )
   }
 
@@ -116,7 +124,7 @@ read_jdx <- function(file = stop("filename is needed"), SOFC = TRUE, debuglevel 
     spc <- new("hyperSpec",
       spc = temp_spc,
       wavelength = list_jdx[[4]]$x,
-      labels = list(.wavelength = x_units, spc = y_units, filename = file)
+      labels = list(.wavelength = x_units, spc = y_units)
     )
   } else {
     stop(
--- tests/testthat/test-read_jdx.R
+++ tests/testthat/test-read_jdx.R
@@ -1,25 +1,52 @@
 # File created by roxut; edit the function definition file, not this file
 
-# test found in read_jdx.R:38 (file:line)
+# test found in read_jdx.R:35 (file:line)
  
 
-# get data files
 sbo <- system.file("extdata", "SBO.jdx", package = "readJDX")
-pcrf <- system.file("extdata", "PCRF.jdx", package = "readJDX")
-pcrf_265 <- system.file("extdata", "PCRF_line265.jdx", package = "readJDX")
-isasspc1 <- system.file("extdata", "isasspc1.dx", package = "readJDX")
 
 test_that("SBO.jdx (IR spectrum) can be imported", {
   expect_silent(spc <- read_jdx(sbo))
-  expect_equal(dim(spc), c(nrow = 1L, ncol = length(colnames(spc)), nwl = 1868L))
 })
+
+test_that("SBO.jdx (IR spectrum) dimensions are correct", {
+  spc <- read_jdx(sbo)
+  expect_equal(nwl(spc), 1868L)
+  expect_equal(nrow(spc), 1L)
+  expect_equal(ncol(spc), 2L)
+})
+
+test_that("SBO.jdx (IR spectrum) labels are correct", {
+  spc <- read_jdx(sbo)
+  expect_equal(spc@label$.wavelength, expression("1/CM"))
+  expect_equal(spc@label$spc, expression("TRANSMITTANCE"))
+  expect_equal(spc@label$filename, "filename")
+})
+
+pcrf <- system.file("extdata", "PCRF.jdx", package = "readJDX")
 
 test_that("PCRF.jdx (real & imaginary 1H NMR spectrum) can be imported", {
   expect_silent(spc <- read_jdx(pcrf))
-  expect_equal(dim(spc), c(nrow = 2L, ncol = length(colnames(spc)), nwl = 7014L))
+})
+
+test_that("PCRF.jdx (real & imaginary 1H NMR spectrum) dimensions are correct", {
+  spc <- read_jdx(pcrf)
+  expect_equal(nwl(spc), 7014L)
+  expect_equal(nrow(spc), 2L)
+  expect_equal(ncol(spc), 2L)
+})
+
+test_that("PCRF.jdx (real & imaginary 1H NMR spectrum) labels are correct", {
+  spc <- read_jdx(pcrf)
+  expect_equal(spc@label$.wavelength, expression())
+  expect_equal(spc@label$spc, expression())
+  expect_equal(spc@label$filename, "filename")
 })
 
 # next test is for a corrupted file, which on readJDX throws an error
+
+pcrf_265 <- system.file("extdata", "PCRF_line265.jdx", package = "readJDX")
+
 test_that("Error is thrown when corrupt file is imported", {
   expect_output(
   expect_error(spc <- read_jdx(pcrf_265)),
@@ -29,17 +56,14 @@ test_that("Error is thrown when corrupt file is imported", {
 
 # next test is a 2D NMR file which readJDX imports fine, but
 # length(list_jdx) != 4 or 5, which is error condition for read_jdx
+# readJDX will issue a warning first, and then read_jdx throws an error
+
+isasspc1 <- system.file("extdata", "isasspc1.dx", package = "readJDX")
+
 test_that("Error is thrown when 2D NMR file is imported", {
-  expect_warning(
-  expect_error(spc <- read_jdx(isasspc1)),
-  "Looks like 2D NMR but could not identify vendor"
-  )
+expect_warning(
+expect_error(spc <- read_jdx(isasspc1)),
+"Looks like 2D NMR but could not identify vendor"
+)
 })
 
-test_that("Labels are correct", {
-  expect_silent(spc <- read_jdx(sbo))
-  expect_length(labels(spc), 3)
-  expect_equal(labels(spc, "filename"), "filename")
-  expect_equal(as.character(labels(spc, "spc")), "TRANSMITTANCE") # y units
-  expect_equal(as.character(labels(spc, ".wavelength")), "1/CM") # x units
-})
